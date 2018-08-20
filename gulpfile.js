f62695df6a57ea2afc3d4a6f6aa754f586ee434c
John Lu <johnluchn@gmail.com>
2018-08-20 14:01:25 +0000

    fixed

--- gulpfile.js
+++ gulpfile.js
@@ -18,13 +18,19 @@ var JS = 'public/js/';
 // 压缩 CSS 文件
 gulp.task('minifycss', function() {
     return gulp.src('public/css/*.css')
+        // 输出一个未压缩的版本
+        .pipe(gulp.dest(CSS))
         .pipe(minifycss())
+        .pipe(rename({extname:'.min.css'}))
         .pipe(gulp.dest(CSS));
 });
 // 压缩 JS 文件
 gulp.task('minify', function() {
     return gulp.src('public/js/*.js')
+        // 输入一个未压缩版本
+        .pipe(gulp.dest(JS))
         .pipe(uglify())
+        .pipe(rename({extname:'.min.js'}))
         .pipe(gulp.dest(JS));
 });
 
